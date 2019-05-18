27e14f836fceb394162e84e90727abaa60823f1c
Xin Chen <xinchen921005@gmail.com>
2019-05-17 19:54:21 -0700

    define three basic components of app component structure

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Highlight from './Highlight.jsx';
+import Gallery from "./Gallery.jsx";
+import Description from "./Description.jsx";
 
-const App= () => 
-  <div>
-    This is the App body!
-  </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Highlight/>
+        <Gallery/>
+        <Description/>
+      </div>
+    )
+  }
+  
+}
 
 
 export default App;
---
+++ client/src/components/Gallery.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Gallery = () =>
+  <div>Gallery</div>
+
+
+export default Gallery;
