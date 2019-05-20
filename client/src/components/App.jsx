71f4dd020f031d6f5ef1906449cf9ea48a87d95b
Xin Chen <xinchen921005@gmail.com>
2019-05-20 15:33:42 -0400

    redifined basic view of app component,added simple styles

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import Highlight from './Highlight.jsx';
 import Gallery from "./Gallery.jsx";
 import Description from "./Description.jsx";
+import styled from 'styled-components';
 
+
+
+const Wrapper = styled.section`
+    padding: 0px 0px 12px;
+    background: #C6D4DF;
+    margin: 0px 231.5px;
+    width: 940px;
+    height: 444px;
+  `;
+
+ const DescWrapper = styled.div`
+    float: right;
+    width: 324px;
+    height: 100%;
+    background: blue;
+ `;
+
+ const GalleryWrapper = styled.div`
+    float: left;
+    width: 616px;
+    height: 100%;
+    background: yellow;
+ `;
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -10,11 +33,14 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <Highlight/>
-        <Gallery/>
-        <Description/>
-      </div>
+      <Wrapper>
+        <GalleryWrapper>
+          <Gallery/>
+        </GalleryWrapper>
+        <DescWrapper>
+          <Description/>
+        </DescWrapper>
+      </Wrapper>
     )
   }
   
